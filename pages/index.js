586cf44506b84ad42467c4b35dc52fbbb60e7291
Devin Ford <devindford@att.net>
2021-01-15 01:22:11 -0500

    added twitter button

--- pages/index.js
+++ pages/index.js
@@ -34,13 +34,17 @@ export default function Home() {
 
       <footer className={styles.footer}>
         <a
-          href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-          target='_blank'
-          rel='noopener noreferrer'
+          href='https://twitter.com/devindford?ref_src=twsrc%5Etfw'
+          class='twitter-follow-button'
+          data-show-count='false'
         >
-          Powered by{' '}
-          <img src='/vercel.svg' alt='Vercel Logo' className={styles.logo} />
+          Follow @DevinDFord
         </a>
+        <script
+          async
+          src='https://platform.twitter.com/widgets.js'
+          charset='utf-8'
+        ></script>
       </footer>
     </div>
   );
